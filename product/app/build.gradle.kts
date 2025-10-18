4d3a58f93b54a75d8c26a6db23904fdd9afef53c
LeaneMatejko <zlac239@live.rhul.ac.uk>
2025-10-18 11:21:32 +0100

    Updated updated gitlab-ci.yaml to fix pipeline.

--- product/app/build.gradle.kts
+++ product/app/build.gradle.kts
@@ -65,6 +65,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation("androidx.compose.compiler:compiler:1.5.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
