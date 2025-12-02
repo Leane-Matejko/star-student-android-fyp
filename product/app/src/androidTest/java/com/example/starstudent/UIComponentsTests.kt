a91853816e098b66ad2113660c2a18305b2da6b3
Leane Matejko <zlac239@live.rhul.ac.uk>
2025-12-02 07:24:40 +0000

    Added UI test for the background render.

--- product/app/src/androidTest/java/com/example/starstudent/UIComponentsTests.kt
+++ product/app/src/androidTest/java/com/example/starstudent/UIComponentsTests.kt
@@ -3,17 +3,28 @@ package com.example.starstudent
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
-import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.starstudent.signInRegister.screens.uiComponents.Background
 import com.example.starstudent.signInRegister.screens.uiComponents.avatarWindow
+import com.example.starstudent.signInRegister.screens.uiComponents.mediumIconWidget
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 
 //@RunWith(AndroidJUnit4::class)
 class UIComponentsTests {
 
     @get:Rule
     val composeTest = createComposeRule()
+
+    @Test
+    fun testBackgroundRender(){
+        composeTest.setContent {
+            Background()
+        }
+
+        composeTest
+            .onNodeWithTag("background")
+            .assertIsDisplayed()
+    }
 
     @Test
     fun testAvatarWindowRender(){
@@ -24,7 +35,5 @@ class UIComponentsTests {
         composeTest
             .onNodeWithTag("avatarWindow")
             .assertIsDisplayed()
-
-
     }
 }
--- product/app/src/main/java/com/example/starstudent/signInRegister/screens/uiComponents/UIComponents.kt
+++ product/app/src/main/java/com/example/starstudent/signInRegister/screens/uiComponents/UIComponents.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -39,7 +38,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.starstudent.ui.theme.LightPink
 @Composable
